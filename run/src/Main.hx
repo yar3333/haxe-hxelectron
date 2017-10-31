f4ba7c78c2e768bb834718c7094f0d1588966d45
Yaroslav Sivakov <yar3333@gmail.com>
2017-10-31 23:51:52 +0300

    * init+

--- run/src/Main.hx
+++ run/src/Main.hx
@@ -7,82 +7,62 @@ using StringTools;
 @:require(sys)
 class Main
 {
-	static inline var API_SOURCE_FILE = 'electron-api.json';
+	static function main()
+	{
+		var file = 'electron-api.json';
+		var out = '../library';
+		
+		if (!FileSystem.exists(file)) error('API file not found [$file]');
+		
+		var pack = ['electron'];
+		var json = Json.parse( File.getContent( file ) );
+		var types = ElectronAPI.build( json, pack );
+		var sourceCode = new StringMap<String>();
+		var printer = new haxe.macro.Printer();
+		//var typePaths = new Array<String>();
 
-	static function main() {
+		for( i in 0...types.length ) {
 
-		var args = Sys.args();
+			var type = types[i];
+			var modulePath = type.pack.join( '.' );
+			var moduleName = type.name;
 
-		switch args[0] {
-
-		//case 'update':
-			//TODO requires ssl
-			//https://api.github.com/repos/electron/electron/releases
-			//https://github.com/electron/electron/releases/download/v${version}/electron-api.json
-
-		default:
-
-			#if macro
-			var file = API_SOURCE_FILE;
-			#else
-			var file = args[0];
-			if( file == null ) file = API_SOURCE_FILE;
-			#end
-
-			if( !FileSystem.exists( file ) )
-				error( 'API file not found [$file]' );
-
-			var out = 'src';
-			var pack = ['electron'];
-			var json = Json.parse( File.getContent( file ) );
-			var types = ElectronAPI.build( json, pack );
-			var sourceCode = new StringMap<String>();
-			var printer = new haxe.macro.Printer();
-			//var typePaths = new Array<String>();
-
-			for( i in 0...types.length ) {
-
-				var type = types[i];
-				var modulePath = type.pack.join( '.' );
-				var moduleName = type.name;
-
-				var doc = '/**';
-				for( item in json ) {
-					if( item.name == type.name ) {
-						if( item.description != null ) doc += '\n\t'+item.description+'\n';
-						if( item.websiteUrl != null ) doc += '\n\tSee: <'+item.websiteUrl+'>';
-						break;
-					}
-				}
-				doc += '\n**/';
-
-				var code = printer.printTypeDefinition( type );
-				var lines = code.split( '\n' );
-				code = lines.shift()+'\n';
-				//code += '\n\n// Electron ${json[0].version}\n';
-				code += '\n'+ doc +'\n';
-				code += lines.join( '\n' );
-				var classPath = modulePath+'.'+moduleName;
-				if( moduleName.endsWith( 'Event' ) ) {
-					code = code.split( '\n' ).slice(1).join( '\n' );
-					sourceCode.set( modulePath, sourceCode.get( modulePath ) +'\n'+code );
-				} else {
-					sourceCode.set( classPath, code );
+			var doc = '/**';
+			for( item in json ) {
+				if( item.name == type.name ) {
+					if( item.description != null ) doc += '\n\t'+item.description+'\n';
+					if( item.websiteUrl != null ) doc += '\n\tSee: <'+item.websiteUrl+'>';
+					break;
 				}
 			}
+			doc += '\n**/';
 
-			for( key in sourceCode.keys() ) {
-				var parts = key.split( '.' );
-				var file = parts.pop();
-				var dir = out + '/' + parts.join( '/' );
-				if( !FileSystem.exists( dir ) ) FileSystem.createDirectory( dir );
-				File.saveContent( dir+'/'+file+'.hx', sourceCode.get( key ) );
+			var code = printer.printTypeDefinition( type );
+			var lines = code.split( '\n' );
+			code = lines.shift()+'\n';
+			//code += '\n\n// Electron ${json[0].version}\n';
+			code += '\n'+ doc +'\n';
+			code += lines.join( '\n' );
+			var classPath = modulePath+'.'+moduleName;
+			if( moduleName.endsWith( 'Event' ) ) {
+				code = code.split( '\n' ).slice(1).join( '\n' );
+				sourceCode.set( modulePath, sourceCode.get( modulePath ) +'\n'+code );
+			} else {
+				sourceCode.set( classPath, code );
 			}
-
-			//File.saveContent( 'all.hxml', typePaths.join( '\n' ) );
-
-			Sys.println( 'Generated [${types.length}] types into [$out]' );
 		}
+
+		for( key in sourceCode.keys() ) {
+			var parts = key.split( '.' );
+			var file = parts.pop();
+			var dir = out + '/' + parts.join( '/' );
+			if( !FileSystem.exists( dir ) ) FileSystem.createDirectory( dir );
+			File.saveContent( dir+'/'+file+'.hx', sourceCode.get( key ) );
+		}
+
+		//File.saveContent( 'all.hxml', typePaths.join( '\n' ) );
+
+		Sys.println( 'Generated [${types.length}] types into [$out]' );
 	}
 
 	static function error( info : String, code = -1 ) {
